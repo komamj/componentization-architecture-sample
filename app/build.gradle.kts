6eedd505e14c57c02328c3a9ab1e2ab582b1224d
komamj <junmaokoma@gmail.com>
2020-12-16 14:42:25 +0800

    feat: [buildSrc] modify appcompat dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,9 +17,9 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-
+    implementation(Dependencies.AndroidX.APPCOMPAT)
+    
     implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.navigation:navigation-fragment:2.3.2")
