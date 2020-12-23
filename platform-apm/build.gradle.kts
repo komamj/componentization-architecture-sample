a19fdbbd58b8d570483f182f2aa9d4aeeeb2bfd7
komamj <junmaokoma@gmail.com>
2020-12-23 15:39:49 +0800

    feat: [app] #N/A rename business module.

---
+++ platform-apm/build.gradle.kts
@@ -0,0 +1,25 @@
+import com.github.komamj.dependency.Dependencies
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("com.github.komamj.common-configuration")
+}
+
+android {
+    defaultConfig {
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation(project(":platform-log"))
+
+    testImplementation(Dependencies.JunitTest.JUNIT)
+    testImplementation(Dependencies.JunitTest.TRUTH)
+    androidTestImplementation(Dependencies.AndroidTest.JUNIT)
+    androidTestImplementation(Dependencies.JunitTest.MOCK_CORE)
+    androidTestImplementation(Dependencies.JunitTest.TRUTH)
+    androidTestImplementation(Dependencies.AndroidTest.ESPRESSO_CORE)
+}
